29e9a4a2af030f649af9825787c6f5d8998c8923
tmflsby <823246840@qq.com>
2020-06-08 15:54:14 +0800

    Initialize project directory structure

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: "App"
+  name: "Breadcrumb"
 }
 </script>
+
+<style scoped>
+
+</style>
