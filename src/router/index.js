38705579daa08b09b1c5c1d6534b499f9b7331c2
tmflsby <823246840@qq.com>
2020-06-24 15:24:46 +0800

    引入driver.js,新增guide指引页面

--- src/router/index.js
+++ src/router/index.js
@@ -69,6 +69,19 @@ export const constantRoutes = [
     ]
   },
   {
+    path: '/guide',
+    component: Layout,
+    redirect: '/guide/index',
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/guide/index'),
+        name: 'Guide',
+        meta: { title: 'Guide', icon: 'guide', noCache: true }
+      }
+    ]
+  },
+  {
     path: '/error',
     component: Layout,
     redirect: 'noRedirect',
